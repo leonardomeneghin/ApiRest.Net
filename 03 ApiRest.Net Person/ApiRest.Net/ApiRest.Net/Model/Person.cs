e2bf85d9df1d0437f707cbf53f4b08f42dd20661
leonardo <leonardozenni@gmail.com>
2021-11-02 10:17:04 -0300

    Implementação Person (with mock)

---
+++ 03 ApiRest.Net Person/ApiRest.Net/ApiRest.Net/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRest.Net.Model
+{
+    public class Person
+    {
+        public long id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string address{ get; set; }
+        public string gender{ get; set; }
+
+    }
+}
---
+++ 03 ApiRest.Net Person/ApiRest.Net/ApiRest.Net/services/implementations/PersonServiceImplementation.cs
@@ -0,0 +1,69 @@
+﻿using ApiRest.Net.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApiRest.Net.services.implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count=0;
+
+        public Person create(Person person)
+        {
+            //Aqui ocorre o acesso à base de dados.
+            return person;
+        }
+
+        public void delete(long id)
+        {
+            
+        }
+
+        public List<Person> findAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++) { //Mocking de persons
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                id = IncrementAndGet() +i,
+                first_name = "Some name" +i,
+                last_name = "Some LastName" +i,
+                address = "Some ADDRESS"+i,
+                gender = "Gender"+i
+            };
+        }
+
+        public Person findByID(long person)
+        {
+            return new Person
+            { 
+                id = IncrementAndGet(),
+                first_name = "Leandro",
+                last_name = "Costa",
+                address = "Uberlandia - Minas Gerais - Brasil",
+                gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person update(Person person)
+        {
+            return person;
+        }
+    }
+}
