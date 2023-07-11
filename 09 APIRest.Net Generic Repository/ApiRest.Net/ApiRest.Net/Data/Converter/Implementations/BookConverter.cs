89d2734fac04eaffa579a3e4ae23d42393627a52
leonardomeneghin <leonardozenni@gmail.com>
2023-07-10 21:35:01 -0300

    Add BookConverter class to bring book to VO pattern

---
+++ 09 APIRest.Net Generic Repository/ApiRest.Net/ApiRest.Net/Data/Converter/Implementations/BookConverter.cs
@@ -0,0 +1,44 @@
+﻿using ApiRest.Net.Data.Converter.Contract;
+using ApiRest.Net.Data.Converter.VO;
+using ApiRest.Net.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiRest.Net.Data.Converter.Implementations
+{
+    public class BookConverter : IParse<Book, BookVO>, IParse<BookVO, Book>
+    {
+        public Book Parse(BookVO item)
+        {
+            var book = new Book()
+            {
+                Id = item.Id,
+                Author = item.Author,
+                Launch_date = item.Launch_date, 
+                Price = item.Price,
+                Title = item.Title
+            };
+            return book;
+        }
+        public BookVO Parse(Book item)
+        {
+            var bookVO = new BookVO
+            {
+                Id =item.Id,    
+                Author = item.Author,
+                Launch_date = item.Launch_date,
+                Price = item.Price,
+                Title = item.Title
+            };
+            return bookVO;
+        }
+        public List<Book> Parse(List<BookVO> itens)
+        {
+            return itens.Select(p =>  Parse(p)).ToList();
+        }
+        public List<BookVO> Parse(List<Book> itens)
+        {
+            return itens.Select(p => Parse(p)).ToList();
+        }
+    }
+}
